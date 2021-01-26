a9f519d7ab139383e26bd15674375334ff00d237
tolis995 <tolis995@users.noreply.github.com>
2021-01-26 18:36:44 +0200

    2

--- src/ge3thema5/Encoder.java
+++ src/ge3thema5/Encoder.java
@@ -1,19 +1,6 @@
 package ge3thema5;
 
 public class Encoder {
-	private DFF k1=new DFF(0,0);
-	private DFF k2=new DFF(0,0);
-	private DFF k3=new DFF(0,0);
-	private int u1, u2;
-	private int c1,c2,c3;
-	
-	public 
-	private void cc1(int u1, int u2) {
-	     if (u1==0) {
-	    	 k1.setNextState(u1);
-	    	 
-	     }
-	}
-	
+
 	
 }
--- src/ge3thema5/MainApp.java
+++ src/ge3thema5/MainApp.java
@@ -1,7 +1,7 @@
 package ge3thema5;
 
 public class MainApp {
-	static int t;
+	static int t,clock;
 	static int[][] G = { {1,1,0,0,0} , {0,1,1,0,0} , {0,0,1,0,1} };
 	static int[] u = {1,2};
 	
@@ -9,21 +9,45 @@ public class MainApp {
 	System.out.println("Γραπτή Εργασία 3 - Θέμα 5 - Συνελικτικοί κώδικες.");
 	System.out.println("----+-----------------------------------------------------");
 	dispHeaders();
-	u[1]=0;
-	u[2]=0;
-	DFF k1=new DFF(0,0);
+	u[0]=0;
+	u[1]=1;
+	DFF k1=new DFF(1,0);
 	DFF k2=new DFF(0,0);
 	DFF k3=new DFF(0,0);
 	
 	int c1=0;
 				
-		for (int i=0;i<G.length;i++) {
-			for (int j=0;j<G[i].length;j++) {
-				System.out.print(G[i][j]+"    ");
-				c1=c1+G[i][j]*u[1];
+//		for (int i=0;i<G.length;i++) {
+//			for (int j=0;j<G[i].length;j++) {
+//				System.out.print(G[i][j]+"    ");
+//				c1=c1+G[i][j]*u[0];
+//				
+//			}
+//			System.out.println();
+//		}
+		
+		for (t=0;t<16;t++) {
+			if ((t % 2)>1) {k1.setD(u[0]);} else {k1.setD(u[1]);}
+			clock=(t % 2);
+//			System.out.println(t+"--"+clock);
+			if (clock==1) {
+				System.out.println((t % 2)+k1.toString()+k2.toString());
+				
+				k2.setQ(k2.getD());
+				k1.setQ(k1.getD());
+				k2.setD(k1.getQ());
+//				k3.setQ(k3.getD());
+				
+//				k3.setD(k1.getQ());
+				
+				
+				
+				
+				
+				
+				
 				
 			}
-			System.out.println();
 		}
 		
 		}
