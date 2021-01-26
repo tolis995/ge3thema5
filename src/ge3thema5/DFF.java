c3241f2fd973c034cbadf5a08d9e45abd041b235
tolis995 <tolis995@users.noreply.github.com>
2021-01-26 18:09:16 +0200

    1

---
+++ src/ge3thema5/DFF.java
@@ -0,0 +1,34 @@
+package ge3thema5;
+
+public class DFF {
+	private int d, q;
+	
+	DFF(int d, int q) {
+		this.d=d;
+		this.q=q;
+	}
+
+	public int getD() {
+		return d;
+	}
+
+	public void setD(int d) {
+		this.d = d;
+	}
+
+	public int getQ() {
+		return q;
+	}
+
+	public void setQ(int q) {
+		this.q = q;
+	}
+
+	@Override
+	public String toString() {
+		return "DFF [D=" + d + ", Q=" + q + "]";
+	}
+
+	
+	
+}
--- src/ge3thema5/MainApp.java
+++ src/ge3thema5/MainApp.java
@@ -1,36 +1,32 @@
 package ge3thema5;
 
 public class MainApp {
-	static int clock;
+	static int t;
 	static int[][] G = { {1,1,0,0,0} , {0,1,1,0,0} , {0,0,1,0,1} };
-	
+	static int[] u = {1,2};
 	
 	public static void main(String[] args) {
 	System.out.println("Γραπτή Εργασία 3 - Θέμα 5 - Συνελικτικοί κώδικες.");
 	System.out.println("----+-----------------------------------------------------");
 	dispHeaders();
-
+	u[1]=0;
+	u[2]=0;
+	DFF k1=new DFF(0,0);
+	DFF k2=new DFF(0,0);
+	DFF k3=new DFF(0,0);
+	
 	int c1=0;
-//	for (clock=0; clock<16; clock++) {
-		// System.out.println(dec2bin(clock));
-//		System.out.println(clock+" |");
-		for (int u1=0;u1<2;u1++) {
-			System.out.println("u1="+u1);
 				
 		for (int i=0;i<G.length;i++) {
 			for (int j=0;j<G[i].length;j++) {
 				System.out.print(G[i][j]+"    ");
-				c1=c1+G[i][j]*u1;
+				c1=c1+G[i][j]*u[1];
 				
 			}
 			System.out.println();
 		}
 		
 		}
-	}
-	
-		
-	}
 	
 	
 	static String dec2bin(int i) {
